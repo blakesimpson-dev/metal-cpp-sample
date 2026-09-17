0feef09ce5ed71ce11b63297e9e39ff8415f50ec
Blake Simpson <blakesimpson.dev@outlook.com>
2026-09-18 01:57:39 +0800

    Load compiled shaders/basic.metallib into an MTL::Library

--- src/main.cc
+++ src/main.cc
@@ -2,10 +2,21 @@
 #include <cassert>
 #include <iostream>
 
+constexpr const char* kMetallibPath = "build/basic.metallib";
+
 int main() {
+  using NS::StringEncoding::UTF8StringEncoding;
+
   MTL::Device* device = MTL::CreateSystemDefaultDevice();
   MTL::CommandQueue* command_queue = device->newCommandQueue();
   assert(command_queue != nullptr && "Failed to create command queue.");
+
+  NS::Error* metallib_error = nullptr;
+  NS::URL* metallib_url = NS::URL::fileURLWithPath(
+      NS::String::string(kMetallibPath, UTF8StringEncoding));
+  MTL::Library* shader_library =
+      device->newLibrary(metallib_url, &metallib_error);
+  assert(shader_library != nullptr && "Failed to create shader library.");
 
   std::cout << "Command Queue created.\n";
   const char* device_name = device->name()->utf8String();
